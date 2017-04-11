1a2742df855be8e216b5876900a8b4e57c078ac3
fidato <fidato.july13@gmail.com>
2017-04-12 00:21:18 +0200

    sites

--- hadoop-edits/startup-vm-centos.sh
+++ hadoop-edits/startup-vm-centos.sh
@@ -46,23 +46,28 @@ cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 ssh -o "StrictHostKeyChecking=no" localhost
 
+#
+cd /opt/hadoop-2.7.3/
+
 #Namenode Format
-/opt/hadoop-2.7.3/bin/hdfs namenode -format
+bin/hdfs namenode -format
+
+echo "Namenode formatted!!"
 
 #start dfs
-/opt/hadoop-2.7.3/sbin/start-dfs.sh
+sbin/start-dfs.sh
 
 #create hdfs directory
-/opt/hadoop-2.7.3/bin/hdfs dfs -mkdir -p /user/trn
+hdfs dfs -mkdir -p /user/trn
 
 #Copy the input files into the distributed filesystem
-/opt/hadoop-2.7.3/bin/hdfs dfs -put /opt/hadoop-2.7.3/etc/hadoop input
+hdfs dfs -put /opt/hadoop-2.7.3/etc/hadoop input
 
 #Run some of the examples provided
-/opt/hadoop-2.7.3/bin/hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.3.jar grep input output 'dfs[a-z.]+'
+hadoop jar share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.3.jar grep input output 'dfs[a-z.]+'
 
 #Copy the output files from the distributed filesystem to the local filesystem and examine them
-/opt/hadoop-2.7.3/bin/hdfs dfs -get output output
+bin/hdfs dfs -get output output
 cat output/*
 
 echo "Script finished!!"
