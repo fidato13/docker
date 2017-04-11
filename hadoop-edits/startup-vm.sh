d281c872ee340ce4da3bd3ccec5a2a4da6e8ca4f
fidato <fidato.july13@gmail.com>
2017-04-11 22:49:11 +0200

    script to get vm up and running for hadoop

---
+++ hadoop-edits/startup-vm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#set -x #echo on
+
+#Refreshing package
+sudo yum install epel-release -y
+
+#Install Java
+sudo yum install -y java-1.8.0-openjdk
+
+echo `java -version`
+
+#Download hadoop binaries
+wget http://www-us.apache.org/dist/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
+
+#Unzip the archive to a designated location
+sudo tar -zxvf hadoop-2.7.3.tar.gz -C /opt
+
+#remove the hadoop-env.sh file
+sudo rm /opt/hadoop-2.7.3/etc/hadoop/hadoop-env.sh
+
+#Download hadoop-env.sh from github
+sudo wget https://raw.githubusercontent.com/fidato13/docker/master/hadoop-edits/hadoop-env.sh -P /opt/hadoop-2.7.3/etc/hadoop/
+
+#You can add the path of the Hadoop program to the PATH environment variable for your convenience
+echo "export PATH=/opt/hadoop-2.7.3/bin:$PATH" | sudo tee -a /etc/profile
+
+#change to sudo
+#sudo -s
+source /etc/profile
+
+#Setup input files
+mkdir ~/source
+cp /opt/hadoop-2.7.3/etc/hadoop/*.xml ~/source
+
+#Run hadoop example
+hadoop jar /opt/hadoop-2.7.3/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.3.jar grep ~/source ~/output 'principal[.]*'
+
+#Output result
+cat ~/output/*
+
+echo "Script finished!!"
