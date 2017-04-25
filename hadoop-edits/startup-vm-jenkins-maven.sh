044bba777980049fd45677d0f52fbffe59163a28
fidato <fidato.july13@gmail.com>
2017-04-25 09:58:53 +0200

    jenkins maven install with java

---
+++ hadoop-edits/startup-vm-jenkins-maven.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#set -x #echo on
+
+#https://tecadmin.net/install-jenkins-in-ubuntu/
+
+#Refreshing package
+sudo apt-get update
+sudo apt-get -y upgrade
+
+#Install Java
+sudo apt-get install default-jdk
+
+#Install Maven
+sudo apt-get install maven
+
+# Install Jenkins on ubuntu with java 
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+# Install Jenkins on ubuntu with java 
+sudo apt-get install jenkins
+
+#Start Jenkins service
+sudo service jenkins restart
+
+#the jenkins will be available at port 8080 of the ip.
+# then you need to enter the secret password on the first login which can be found via the following command
+#sudo less /var/lib/jenkins/secrets/initialAdminPassword
+
