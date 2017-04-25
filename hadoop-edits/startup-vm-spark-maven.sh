7aae750bc45b5e12ee46aa67b134954bb4d07226
fidato <fidato.july13@gmail.com>
2017-04-25 15:15:15 +0200

    clone git and build spark

---
+++ hadoop-edits/startup-vm-spark-maven.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#set -x #echo on
+
+#https://tecadmin.net/install-jenkins-in-ubuntu/
+
+#execute this script inside vm using below command:
+
+#Refreshing package
+sudo apt-get update
+sudo apt-get -y upgrade
+
+#Install Java
+sudo apt-get install default-jdk
+
+#Install Maven
+sudo apt-get install maven
+
+#maven version
+mvn --version
+
+# Install Jenkins on ubuntu with java 
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+# Install Jenkins on ubuntu with java
+sudo apt-get update 
+sudo apt-get install jenkins
+
+#Start Jenkins service
+sudo service jenkins restart
+
+#clone spark
+git clone git://github.com/apache/spark.git -b branch-2.1
+
+#go to spark directory
+cd ~/spark
+
+#build spark
+#./build/mvn -Pyarn -Phadoop-2.7 -Dhadoop.version=2.7.0 -DskipTests clean package
+
+#the jenkins will be available at port 8080 of the ip.
+# then you need to enter the secret password on the first login which can be found via the following command
+#sudo less /var/lib/jenkins/secrets/initialAdminPassword
+
